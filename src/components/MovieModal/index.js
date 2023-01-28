2764a611b8fa62b427793d3275507958f16d6773
권순성 <tnstjd120@gmail.com>
2023-01-28 15:34:31 +0900

    MovieModal finish

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -14,16 +14,18 @@ const ModalBackground = styled.div`
 
 const Modal = styled.div`
   width: 100%;
-  max-width: 500px;
+  max-width: 800px;
   height: 84vh;
   overflow: auto;
-  background-color: #fff;
+  background-color: #111;
   border-radius: 12px;
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
 `
+
+const list = [1, 2, 3, 4, 5]
 
 const MovieModal = ({
   backdrop_path,
@@ -35,10 +37,46 @@ const MovieModal = ({
   vote_average,
   setModalOpen,
 }) => {
+  const bgBaseUrl = 'https://image.tmdb.org/t/p/original/'
+
   return (
-    <ModalBackground onClick={() => setModalOpen(false)}>
+    <ModalBackground>
       <Modal>
-        <div className="modal__banner"></div>
+        <div className="modal__header">
+          <img src={bgBaseUrl + backdrop_path} />
+
+          <button className="modal__close" onClick={() => setModalOpen(false)}>
+            &times;
+          </button>
+        </div>
+
+        <div className="modal__body">
+          <div className="modal__detail">
+            <span>97% 일치</span> {release_date ? release_date : first_air_date}
+          </div>
+
+          <h2 className="modal__title">{title ? title : name}</h2>
+
+          <div className="modal__average">평점 : {vote_average}</div>
+
+          <p className="modal__overview">{overview}</p>
+
+          {/* <div className="modal__list">
+            <h3>
+              <strong>회차</strong>
+              <small>{title ? title : name}</small>
+            </h3>
+
+            <ul>
+              {list.map((item, index) => (
+                <li key={index}>
+                  <h4>{`${item}화`}</h4>
+                  <p>{overview}</p>
+                </li>
+              ))}
+            </ul>
+          </div> */}
+        </div>
       </Modal>
     </ModalBackground>
   )
